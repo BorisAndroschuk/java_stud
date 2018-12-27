29f98a32e53491f05bd9c9e5a58a8cd93cee0ee9
band <band@gendalf.ru>
2018-12-27 23:03:04 +0300

    text taks

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String[] roles = new String[] {"Городничий","Аммос Федорович","Артемий Филиппович","Лука Лукич","Аммос Левый"};
+        String[] textLines = new String[] {"Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор."
+                ,"Аммос Федорович: Как ревизор?"
+                ,"Артемий Филиппович: Как ревизор?"
+                ,"Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем."
+                ,"Аммос Федорович: Вот те на!"
+                ,"Артемий Филиппович: Вот не было заботы, так подай!"
+                ,"Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+        StringBuilder result = new StringBuilder();
+        StringBuilder tempSearch = new StringBuilder();
+        int regexLength;
+        for (int i=0;i<roles.length;i++) {
+            //ищем строки только нужной роли
+            String regex = roles[i]+":";
+            result.append(regex+'\n');
+            regexLength = regex.length();
+            for (int j=0;j<textLines.length;j++) {
+                if (textLines[j].startsWith(regex)) {
+                    result.append(""+(j+1)+")"+ textLines[j].substring(regexLength)+'\n');
+                }
+            }
+            result.append('\n');
+        }
+        result.toString();
+        System.out.println("fd");
     }
 }
